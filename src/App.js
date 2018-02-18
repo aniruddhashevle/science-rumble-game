9ea767f36d3708e01c28581e581489f02fa7c356
Aniruddha Shevle <ashevle@instarem.com>
2018-02-18 21:39:14 +0530

    basic science rumble app

---
+++ src/App.js
@@ -0,0 +1,182 @@
+import React, { Component } from 'react';
+import './App.css';
+import {
+	questionsConfig,
+	answersConfig
+} from './data';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			questionsConfig,
+			answersConfig,
+			showQuestion: true,
+			quesSubSequence: 0,
+			quesSequence: 0,
+			congratsMsg: false,
+		};
+		this.isFirstLoad = true;
+		this.currentQuesObj = null;
+		this.quesRelatedAns = [];
+	}
+
+	questionDisplay = () => {
+		let {
+			quesSequence,
+			quesSubSequence,
+			answersConfig
+		} = this.state;
+		if (this.isFirstLoad) {
+			quesSequence = 1;
+			this.isFirstLoad = false;
+		}
+
+		let ques = [];
+		this.state.questionsConfig.forEach((item, index) => {
+			if (item.sequence === quesSequence) {
+				if (quesSubSequence && item.subQuestionsConfig) {
+					item.subQuestionsConfig.forEach(subItem => {
+						if (subItem.subSequence === quesSubSequence) {
+							ques.push(
+								<div
+									key={index}
+									className="ques"
+								>
+									<p>{subItem.subQuestion}</p>
+								</div>
+							);
+							this.currentQuesObj = { ...subItem, sequence: item.sequence };
+							this.quesRelatedAns = [];
+							answersConfig.forEach(ansItem => {
+								if (ansItem.sequence === item.sequence && ansItem.subSequence === subItem.subSequence && !ansItem.isRelatedToSubQues) {
+									this.quesRelatedAns.push(ansItem);
+								}
+							});
+						}
+					});
+				} else {
+					if (item) {
+						ques.push(
+							<div
+								key={index}
+								className="ques"
+							>
+								<p>{item.question}</p>
+							</div>
+						);
+						this.currentQuesObj = item;
+						this.quesRelatedAns = [];
+						answersConfig.forEach(ansItem => {
+							if (ansItem.sequence === item.sequence && (ansItem.isRelatedToSubQues || !ansItem.subSequence)) {
+								this.quesRelatedAns.push(ansItem);
+							}
+						});
+					}
+				}
+			}
+		});
+		return ques;
+	}
+
+	ansClicked = (ansObj) => {
+		if (ansObj.isDisabled) {
+			alert('Wrong Answer');
+			return;
+		} else {
+			if (this.currentQuesObj) {
+				if (this.currentQuesObj.sequence && ansObj.sequence && ansObj.sequence !== this.currentQuesObj.sequence) {
+					alert('Wrong Answer');
+					return;
+				} else {
+					let relatedAns = this.quesRelatedAns.find(item => item.ans === ansObj.ans);
+					if (!relatedAns) {
+						alert('Wrong Answer');
+						return;
+					}
+				}
+			}
+			let quesSequence = ansObj.sequence, quesSubSequence = 0;
+			//sub questions
+			if (ansObj.subSequence) {
+				quesSubSequence = ansObj.subSequence;
+				if (ansObj.isRightAns) {
+					let quesObj = this.state.questionsConfig.find(item => item.sequence === ansObj.sequence);
+					if (quesObj.subQuestionsConfig) {
+						let subQuesObj = quesObj.subQuestionsConfig.find(item => item.subSequence === ansObj.subSequence + 1);
+						if (subQuesObj) {
+							quesSubSequence++;
+						} else {
+							if (quesSequence === this.state.questionsConfig.length) {
+								this.setState({ congratsMsg: true });
+							}
+							quesSubSequence = 0;
+						}
+					}
+				}
+			}
+			let answersConfig = this.state.answersConfig;
+			answersConfig.forEach(item => {
+				if (item.ans === ansObj.ans) item.isDisabled = true;
+			});
+			//main questions
+			if (ansObj.isRightAns && !ansObj.subSequence) {
+				if (quesSequence < this.state.questionsConfig.length) {
+					quesSequence++;
+				}
+				else {
+					this.setState({ congratsMsg: true });
+				}
+			}
+			this.setState({ answersConfig, showQuestion: true, quesSequence, quesSubSequence });
+		}
+	}
+
+	serachAns = () => {
+		this.setState({ showQuestion: false });
+	}
+
+	ansDisplay = () => {
+		let ans = this.state.answersConfig.map((item, index) =>
+			<div
+				key={index}
+				className={`ans-box`}
+				onClick={
+					() => this.ansClicked(item)
+				}
+			>
+				<span>{item.ans}</span>
+			</div>
+		);
+		return ans;
+	}
+
+	render() {
+		const {
+			congratsMsg,
+			showQuestion
+		} = this.state;
+		return (
+			<div className="App">
+				{
+					congratsMsg ?
+						<h1>Congratulations!!!</h1>
+						:
+						showQuestion ?
+							<div className="questions-wrapper">
+								{this.questionDisplay()}
+								<button type="button" onClick={this.serachAns}>Search Answer</button>
+							</div>
+							:
+							<div className="ans-wrapper">
+								{this.ansDisplay()}
+							</div>
+
+				}
+
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,536 @@
+export const questionsConfig = [
+    {
+        question: '1. The fundamental particles present in an atom are electrons and _________',
+        sequence: 1,
+        subQuestionsConfig: [
+            {
+                subQuestion: '1a. Higher the Frequency, Greater will be ',
+                subSequence: 1
+            },
+        ]
+    },
+    {
+        question: '2. Magnitude of displacement from initial position to final position is the',
+        sequence: 2,
+        subQuestionsConfig: [
+            {
+                subQuestion: '2c. An adult consist of 206 bones , so at birth ; how many bones do new born have',
+                subSequence: 1
+            },
+        ]
+    },
+    {
+        question: '3. Tuberculosis affects -- part of the body',
+        sequence: 3,
+        subQuestionsConfig: [
+            {
+                subQuestion: '3c. Cathode rays are stream of ---- charged Particle',
+                subSequence: 1
+            }
+        ]
+    },
+    {
+        question: '4. A matchstick head consists of which substance',
+        sequence: 4,
+        subQuestionsConfig: [
+            {
+                subQuestion: '4c. The colored part of human eye that controls how much light passes through the pupil is called ',
+                subSequence: 1
+            },
+            {
+                subQuestion: '4cb. Rainbow is formed due to --- of sunlight by water droplets',
+                subSequence: 2
+            }
+        ]
+    },
+    {
+        question: '5. Product of mass and velocity is known as',
+        sequence: 5,
+        subQuestionsConfig: [
+            {
+                subQuestion: '5c. Type of connective tissue present in the ends of bones and joints is',
+                subSequence: 1
+            },
+            {
+                subQuestion: '5cb. Applying pressure and reducing temperature of a gas can lead to',
+                subSequence: 2
+            }
+        ]
+    },
+    {
+        question: '6. The method by which desired characters of 2 plants can be combined is',
+        sequence: 6,
+        subQuestionsConfig: [
+            {
+                subQuestion: '6a. The speed of sound is maximum in ________and Minimum in ____________.',
+                subSequence: 1
+            },
+            {
+                subQuestion: '6ab. _______ is produced when an acid solution is mixed with basic solution',
+                subSequence: 2
+            }
+        ]
+    },
+    {
+        question: '7. Instrument used for measurement of Humidity in air  ?',
+        sequence: 7,
+        subQuestionsConfig: [
+            {
+                subQuestion: '7a. Calcium hydroxide is also known as ?',
+                subSequence: 1
+            },
+            {
+                subQuestion: '7ab. If your entire digestive system is straightened, How long would it be ?',
+                subSequence: 2
+            }
+        ]
+    }
+];
+
+// Math.floor((Math.random() * 63) + 1);
+
+export const answersConfig = [
+    //Ans 1
+    {
+        ans: 'Nucleons',
+        sequence: 1,
+        randomSequence: 1,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Neutrons',
+        sequence: 1,
+        randomSequence: 2,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Protons',
+        sequence: 1,
+        subSequence: 1,
+        randomSequence: 3,
+        isRightAns: false,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: 'Pitch',
+        sequence: 1,
+        randomSequence: 4,
+        subSequence: 1,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Loudness',
+        sequence: 1,
+        randomSequence: 5,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Amplitude',
+        sequence: 1,
+        randomSequence: 6,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false
+    },
+
+    //Ans 2
+    {
+        ans: 'Straight Line',
+        sequence: 2,
+        randomSequence: 7,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Tangent',
+        sequence: 2,
+        randomSequence: 8,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Total Distance',
+        sequence: 2,
+        randomSequence: 9,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: '350',
+        sequence: 2,
+        randomSequence: 10,
+        subSequence: 1,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: '250',
+        sequence: 2,
+        randomSequence: 11,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: '206',
+        sequence: 2,
+        subSequence: 1,
+        randomSequence: 12,
+        isRightAns: false,
+        isDisabled: false
+    },
+
+    //Ans 3
+    {
+        ans: 'Lungs',
+        sequence: 3,
+        randomSequence: 13,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Kidney',
+        sequence: 3,
+        randomSequence: 14,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Liver',
+        sequence: 3,
+        subSequence: 1,
+        randomSequence: 15,
+        isRightAns: false,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: 'Negative',
+        sequence: 3,
+        randomSequence: 16,
+        subSequence: 1,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Positive',
+        sequence: 3,
+        randomSequence: 17,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Neutral',
+        sequence: 3,
+        randomSequence: 18,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+
+    //Ans 4
+    {
+        ans: 'Phosphorous',
+        sequence: 4,
+        randomSequence: 22,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Wood',
+        sequence: 4,
+        randomSequence: 23,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Carbon',
+        sequence: 4,
+        subSequence: 1,
+        randomSequence: 24,
+        isRightAns: false,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: 'Iris',
+        sequence: 4,
+        randomSequence: 25,
+        subSequence: 1,
+        isRightAns: true,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: 'Retina',
+        sequence: 4,
+        randomSequence: 26,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Cornea',
+        sequence: 4,
+        randomSequence: 27,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false,
+    },
+    {
+        ans: 'Dispersion',
+        sequence: 4,
+        randomSequence: 28,
+        subSequence: 2,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Diffraction',
+        sequence: 4,
+        randomSequence: 29,
+        subSequence: 2,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Scattering',
+        sequence: 4,
+        randomSequence: 30,
+        subSequence: 2,
+        isRightAns: false,
+        isDisabled: false
+    },
+
+    //Ans 5
+    {
+        ans: 'Momentum',
+        sequence: 5,
+        randomSequence: 23,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Impulse',
+        sequence: 5,
+        randomSequence: 25,
+        isRightAns: false,
+        isDisabled: false,
+    },
+    {
+        ans: 'Kinetic Energy',
+        sequence: 5,
+        randomSequence: 25,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: 'Cartilage',
+        sequence: 5,
+        randomSequence: 22,
+        subSequence: 1,
+        isRightAns: true,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: 'Tendon',
+        sequence: 5,
+        randomSequence: 26,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Ligament',
+        sequence: 5,
+        randomSequence: 27,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false,
+    },
+    {
+        ans: 'Liquefication',
+        sequence: 5,
+        randomSequence: 28,
+        subSequence: 2,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Evaporation',
+        sequence: 5,
+        randomSequence: 29,
+        subSequence: 2,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Diffusion',
+        sequence: 5,
+        randomSequence: 30,
+        subSequence: 2,
+        isRightAns: false,
+        isDisabled: false
+    },
+
+    //Ans 6
+    {
+        ans: 'Grafting',
+        sequence: 6,
+        randomSequence: 23,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Budding',
+        sequence: 6,
+        randomSequence: 26,
+        isRightAns: false,
+        isDisabled: false,
+    },
+    {
+        ans: 'Cutting',
+        sequence: 6,
+        randomSequence: 26,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: 'Solid ,Gas',
+        sequence: 6,
+        randomSequence: 22,
+        subSequence: 1,
+        isRightAns: true,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: 'Gas, Solid',
+        sequence: 6,
+        randomSequence: 26,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Liquid, Solid',
+        sequence: 6,
+        randomSequence: 27,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false,
+    },
+    {
+        ans: 'Water',
+        sequence: 6,
+        randomSequence: 28,
+        subSequence: 2,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Carbondioxide',
+        sequence: 6,
+        randomSequence: 29,
+        subSequence: 2,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Oxygen',
+        sequence: 6,
+        randomSequence: 30,
+        subSequence: 2,
+        isRightAns: false,
+        isDisabled: false
+    },
+
+    //Ans 7
+    {
+        ans: 'Hygrometer',
+        sequence: 7,
+        randomSequence: 23,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: 'Barometer',
+        sequence: 7,
+        randomSequence: 27,
+        isRightAns: false,
+        isDisabled: false,
+    },
+    {
+        ans: 'Manometer',
+        sequence: 7,
+        randomSequence: 27,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: 'Lime water',
+        sequence: 7,
+        randomSequence: 22,
+        subSequence: 1,
+        isRightAns: true,
+        isDisabled: false,
+        isRelatedToSubQues: true,
+    },
+    {
+        ans: 'Lime salt',
+        sequence: 7,
+        randomSequence: 27,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: 'Soda',
+        sequence: 7,
+        randomSequence: 27,
+        subSequence: 1,
+        isRightAns: false,
+        isDisabled: false,
+    },
+    {
+        ans: '30 feet ( 9 metre)',
+        sequence: 7,
+        randomSequence: 28,
+        subSequence: 2,
+        isRightAns: true,
+        isDisabled: false
+    },
+    {
+        ans: '10 feet (3 metre)',
+        sequence: 7,
+        randomSequence: 29,
+        subSequence: 2,
+        isRightAns: false,
+        isDisabled: false
+    },
+    {
+        ans: '50Feet (15 metre)',
+        sequence: 7,
+        randomSequence: 30,
+        subSequence: 2,
+        isRightAns: false,
+        isDisabled: false
+    },
+];
